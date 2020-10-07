8a78d6c6e515e8cf55f2b2d252a344804536fe9c
JKafka97 <67194333+jkafka97@users.noreply.github.com>
2020-10-07 18:32:26 +0200

    project update

--- Bulls_and_Cows.py
+++ Bulls_and_Cows.py
@@ -1,6 +1,5 @@
 def bulls_and_cows(number):
 
-
 print("Hi there!\nI've generated a random 4 digit number for you.\nLet's play a bulls and cows game.")
 print("Enter a number")
 input_number = input()
