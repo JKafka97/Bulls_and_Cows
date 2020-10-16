db61c2c374ad4ca926a0670a24f2aa923de9db78
JKafka97 <67194333+jkafka97@users.noreply.github.com>
2020-10-16 11:08:24 +0200

    fnc make

--- Bulls_and_Cows.py
+++ Bulls_and_Cows.py
@@ -6,57 +6,73 @@ Let's play a bulls and cows game.
 Enter a number''')
 
 
-# random_number
-
 def random_number():
     number = set()
     while len(number) < 4:
         number.add(random.randrange(1, 10))
-    my_random_number = ""
+    complete_random_number = ""
     for num in number:
-        my_random_number += str(num)
-    return my_random_number
+        complete_random_number += str(num)
+    return complete_random_number
 
 
-# game_loop
-
-guess = 1
-while True:
-    bulls_cows = {"bulls": 0, "cows": 0}
-    input_number = input()
+def input_control(input_number):
     if len(input_number) != 4 or not input_number.isdigit():
         print("Your number has to be four-digit number")
-        continue
-    n = 0
-    for bulls in random_number():
-        if bulls == input_number[n]:
-            bulls_cows["bulls"] += 1
-        if bulls in input_number:
-            bulls_cows["cows"] += 1
-        n += 1
+    else:
+        return input_number
+
+
+def bulls_cows_control(bull_s, cow_s):
+    guess = 1
+    bulls_cows = {"bulls": 0, "cows": 0}
+    while bulls_cows["bulls"] != 4 and bulls_cows["cows"] != 4:
+        input_number = input()
+        input_control(input_number)
+        n = 0
+        for bulls in random_number():
+            if bulls == input_number[n]:
+                bulls_cows["bulls"] += 1
+            if bulls in input_number:
+                bulls_cows["cows"] += 1
+            n += 1
+        guess += 1
+        print("{} bull{}, {} cow{}".format(bulls_cows["bulls"], bull_s, bulls_cows["cows"], cow_s))
+    return bulls_cows
+
+
+def guess_multiple(guess):
+
+    if guess == 1:
+        multi = ""
+    else:
+        multi = "es"
+    return multi
+
+
+def correct_ans(bulls_cows, guess, multi):
     if bulls_cows["bulls"] == 4 and bulls_cows["cows"] == 4:
-        if guess == 1:
-            multi = ""
-        else:
-            multi = "es"
         print("Correct, you've guessed the right number in {} guess{}!".format(guess, multi))
-        file = open('score.txt', 'a+')
-        file.write("{},".format(str(guess)))
-        file.seek(0)
-        score = ((file.read()).strip(",")).split(",")
-        my_sum = 0
-        for numbers in score:
-            my_sum += int(numbers)
-        average = my_sum / len(score)
-        print("Average score is {}".format(average))
-        break
+
+
+def bulls_multiple(bulls_cows):
     bull_s, cow_s = "s", "s"
-    template = "{} bull{}, {} cow{}"
     if bulls_cows["bulls"] == 1 and bulls_cows["cows"] == 1:
         bull_s, cow_s = "", ""
     if bulls_cows["bulls"] == 1:
         bull_s = ""
     if bulls_cows["cows"] == 1:
         cow_s = ""
-    print(template.format(bulls_cows["bulls"], bull_s, bulls_cows["cows"], cow_s))
-    guess += 1
+    return bull_s, cow_s
+
+
+def ans_write(guess):
+    file = open('score.txt', 'a+')
+    file.write("{},".format(str(guess)))
+    file.seek(0)
+    score = ((file.read()).strip(",")).split(",")
+    my_sum = 0
+    for numbers in score:
+        my_sum += int(numbers)
+    average = my_sum / len(score)
+    print("Average score is {}".format(average))
