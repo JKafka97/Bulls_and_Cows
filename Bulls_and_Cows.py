ec6f44ca56d65deb3bddbf440c79d1e490f0b8aa
JKafka97 <67194333+jkafka97@users.noreply.github.com>
2020-10-17 12:03:53 +0200

    final version

--- Bulls_and_Cows.py
+++ Bulls_and_Cows.py
@@ -10,12 +10,11 @@ Enter a number''')
 
 def random_number():
     number = set()
-    while len(number) < 3:
+    while len(number) < 4:
         number.add(random.randrange(0, 10))
     complete_random_number = ""
     for num in number:
         complete_random_number += str(num)
-    complete_random_number = str(random.randrange(1, 10))+complete_random_number
     return complete_random_number
 
 
